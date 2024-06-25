af07604ae47834260bc825915a6216126f9e99e1
V4N1LLA-1CE <austinsofaer@gmail.com>
2024-06-25 21:28:08 +0700

    fix: change image path for pfpin order to avoid conflicts with gh-page

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,11 @@ const Card = ({ id, username, email }) => {
   return (
     <div className="shadow-lg shadow-slate-300 text-center text-slate-500 py-20 rounded-xl w-72 hover:scale-105 transition-all">
       <div className="rounded-full w-20 h-20 border border-dashed border-slate-500 overflow-hidden m-auto">
-        <img src="/profile-icon.jpg" alt="pfp" className="object-contain p-2" />
+        <img
+          src="./dev-sphere/profile-icon.jpg"
+          alt="pfp"
+          className="object-contain p-2"
+        />
       </div>
       <div className="overflow-x-clip p-5 text-xs">
         <h2>{username}</h2>
