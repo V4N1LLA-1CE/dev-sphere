b8f407446c2a7375ec82c3af76659418eb3ae23d
V4N1LLA-1CE <austinsofaer@gmail.com>
2024-06-25 20:24:47 +0700

    fix: refactor code

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import CardList from "./CardList";
-import { devs } from "./dev-info";
-import SearchBox from "./SearchBox";
-import Scroll from "./Scroll";
+import CardList from "../components/CardList";
+import { devs } from "../dev-info";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
 
 class App extends Component {
   constructor() {
