68cf91a564a249e16b93286b1d94e0e698e19816
V4N1LLA-1CE <austinsofaer@gmail.com>
2024-06-25 22:09:04 +0700

    fix: fix pfp not showing up

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,11 @@
 import React from "react";
+import pfp from "../images/profile-icon.jpg";
 
 const Card = ({ id, username, email }) => {
   return (
     <div className="shadow-lg shadow-slate-300 text-center text-slate-500 py-20 rounded-xl w-72 hover:scale-105 transition-all">
       <div className="rounded-full w-20 h-20 border border-dashed border-slate-500 overflow-hidden m-auto">
-        <img
-          src="../images/profile-icon.jpg"
-          alt="pfp"
-          className="object-contain p-2"
-        />
+        <img src={pfp} alt="pfp" className="object-contain p-2" />
       </div>
       <div className="overflow-x-clip p-5 text-xs">
         <h2>{username}</h2>
