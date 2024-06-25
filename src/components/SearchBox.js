de2e3763c0147f150136622890af9c35bf4c32fb
V4N1LLA-1CE <austinsofaer@gmail.com>
2024-06-25 22:14:05 +0700

    fix: add padding to right side of searchbar

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,7 +4,7 @@ const SearchBox = ({ searchChange }) => {
   return (
     <section className="py-2 text-center my-0 mx-auto">
       <input
-        className="py-4 pl-5 rounded-3xl bg-slate-50 border-2 border-solid border-slate-300 w-72"
+        className="py-4 pl-5 rounded-3xl bg-slate-50 border-2 border-solid border-slate-300 w-72 pr-2"
         type="search"
         placeholder="Search for Developers"
         onChange={searchChange}
