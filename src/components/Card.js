547fd6f9f0be4b2bdf90088fd7de4198f49cda74
V4N1LLA-1CE <austinsofaer@gmail.com>
2024-06-25 21:35:42 +0700

    fix: move pfp-icon to new directory (src/images/) and change image path in card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ const Card = ({ id, username, email }) => {
     <div className="shadow-lg shadow-slate-300 text-center text-slate-500 py-20 rounded-xl w-72 hover:scale-105 transition-all">
       <div className="rounded-full w-20 h-20 border border-dashed border-slate-500 overflow-hidden m-auto">
         <img
-          src="./dev-sphere/profile-icon.jpg"
+          src="../images/profile-icon.jpg"
           alt="pfp"
           className="object-contain p-2"
         />
