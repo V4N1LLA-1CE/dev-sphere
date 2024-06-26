6b4732fcb2cc9b0ceb6a3f2692d65b665690dbb1
V4N1LLA-1CE <austinsofaer@gmail.com>
2024-06-26 23:10:52 +0700

    style: update style for no search result indicator

--- src/components/EmptyListError.js
+++ src/components/EmptyListError.js
@@ -1,11 +1,20 @@
 import React from "react";
+import sad from "../images/sad-face.png";
 
 const EmptyListError = ({ devs, children }) => {
   if (devs.length === 0) {
     return (
-      <h1 className="text-slate-300 font-semibold text-2xl text-center">
-        Sorry, We couldn't find any results
-      </h1>
+      <>
+        <img
+          src={sad}
+          alt="sad-face"
+          className="object-scale-down m-auto w-64 pt-[10rem]"
+        />
+        <h1 className="text-red-500 font-semibold text-2xl text-center opacity-50 m-auto pt-5">
+          Sorry, we couldn't find any results{" "}
+          <span className="inline-block">:(</span>
+        </h1>
+      </>
     );
   } else {
     return children;
--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Scroll = (props) => {
   return (
-    <div className="overflow-y-scroll border border-slate-300 h-screen m-10">
+    <div className="overflow-y-scroll border border-slate-300 h-[45rem] m-10">
       {props.children}
     </div>
   );
